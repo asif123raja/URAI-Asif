70d2d6f1c44ee6e25264c4c6f18147c88e2bd3bf
Md Asiful Ameen <asifulameen208@gmail.com>
2025-03-16 06:42:22 +0530

    signin

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-900 text-white p-4 flex justify-between items-center shadow-md relative w-full">
       {/* Logo */}
-      <Link href="/" className=" text-2xl font-bold">
+      <Link href="/" className=" text-3xl font-bold">
         <span className="text-blue-500">UR</span>
         <span className="text-gray-400">AI</span>
       </Link>
@@ -65,17 +65,8 @@ export default function Navbar() {
       </div>
 
       {/* Profile / Sign-in */}
-      <div className="hidden md:flex">
-        {isLoggedIn ? (
-          <Link href="/" className="flex items-center gap-2 hover:text-blue-400">
-            <UserCircle className="w-6 h-6" /> Profile
-          </Link>
-        ) : (
-          <Button onClick={() => setIsLoggedIn(true)} variant="outline" className=" ">
-            {/* <LogIn className="w-5 h-5 mr-2" /> Sign In */}
-          </Button>
-        )}
-      </div>
+      
     </nav>
   );
 }
+
